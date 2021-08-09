1944d010bb8673a3c5674c45b179c6e26957f111
balasundarambui <56162438+balasundarambui@users.noreply.github.com>
2021-08-09 15:54:11 +0530

    edit_school page design

--- application/views/themes/default.php
+++ application/views/themes/default.php
@@ -21,6 +21,7 @@
 
         <!-- Le styles -->  <!-- Bootstrap CSS -->       
         <link rel="stylesheet" href="<?php echo base_url('assets/admin/css/bootstrap.min.css'); ?>" >
+        <link rel="stylesheet" href="<?php echo base_url('assets/admin/css/custom.css'); ?>" >
 
 
         <!--<link rel="stylesheet" href="<?php // echo base_url('assets/admin/fontawesome-free-5.0.8/web-fonts-with-css/css/fontawesome-all.min.css');      ?>" >-->
