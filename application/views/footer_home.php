3f546304465405eaf1f5f95538dbfb4c882b2728
balasundarambui <56162438+balasundarambui@users.noreply.github.com>
2021-09-06 16:48:53 +0530

    Home page new layout design

--- application/views/footer_home.php
+++ application/views/footer_home.php
@@ -1,59 +1,68 @@
 <div class="content-footer mat-80 section-pad">
-    <div class="container">
-        <div class="heading-collapse">Edugate-in is to inter-connect schools, parents and education community on a single platform to create mutual benefit. Let's Join with us to find your information like where your school located, what are the facilities and promotional offers in Edugate-india.</div><br>
-
-        <div class="heading-collapse">Edugatein offer a detailed and <h5>well-researched guide about schools</h5> type, curriculum, facilities, activity classes, contact details and location through map. This information is directly from school management. In particular, we allocate the separate page for the schools in all over tamilnadu to provide the details of the school. By this information parents can choose schools in efficient manner and school management also gives all their unique information.</div> <br>
-
-        <div class="heading-collapse">If you are looking for the <h5>best schools in <?php echo $city; ?></h5>, We have <h5>Top and Best schools lists</h5> are available here and also to find the best CBSE, International, Matriculation, State Board and Kindergarten in the right place. Our goal is to develop creative skills and extra-curricular activities such as sports, dance, music, swimming, yoga and much more.</div>
-
+    <div class="container-fluid">      
         <!-- <h6>Some of our services that will prove that we are best:</h6> -->
-
         <div class="content-footer-list">
             <div class="row">
-                <div class="col-lg-4 mab-20">
-                    <h5 class="mb-2">CBSE Schools</h5>
-                    <div class="heading-collapse">The CBSE carries out continuous research and development for the advancement of the academic standards in the country. It has recently introduced the Grading system based on continuous and comprehensive evaluation. Here choose <h6>best CBSE schools in <?php echo $city; ?>.</h6></div>
+                <div class="col">
+                    <div class="footer-list-inner">
+                        <div class="footer-list-icon"><img src="<?php echo base_url() ?>assets/front/images/icons/f-1.png" alt="best kindergarten schools in <?php echo $city; ?>" /></div>
+                        <h5 class="mb-2">CBSE Schools</h5>
+                        <div class="heading-collapse">The CBSE carries out continuous research and development for the advancement of the academic standards in the country. It has recently introduced the Grading system based on continuous and comprehensive evaluation. Here choose <h6>best CBSE schools in <?php echo $city; ?>.</h6></div>
+                    </div>
                 </div>
 
-                <div class="col-lg-4 mab-20">
-                    <h5 class="mb-2">International Schools</h5>
-                    <div class="heading-collapse">An international school is a school that promotes international education, in an international environment, either by adopting IGCSE, IB and Edexcal. Choose interested board from <h6>best International Schools in <?php echo $city; ?>.</h6></div>
+                <div class="col">
+                    <div class="footer-list-inner">
+                        <div class="footer-list-icon"><img src="<?php echo base_url() ?>assets/front/images/icons/f-2.png" alt="best kindergarten schools in <?php echo $city; ?>" /></div>
+                        <h5 class="mb-2">International Schools</h5>
+                        <div class="heading-collapse">An international school is a school that promotes international education, in an international environment, either by adopting IGCSE, IB and Edexcal. Choose interested board from <h6>best International Schools in <?php echo $city; ?>.</h6></div>
+                    </div>
                 </div>
 
-                <div class="col-lg-4 mab-20">
-                    <h5 class="mb-2">Matriculation Schools</h5>
-                    <div class="heading-collapse">Schools use English as the language of instruction. They have a unique curriculum until class 10 and follow the <?php echo $city; ?> State Board curriculum for classes 11 and 12. You can choose <h6>best Matriculation schools in <?php echo $city; ?>.</h6></div>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col-lg-4 mab-20">
-                    <h5 class="mb-2">Special Schools</h5>
-                    <div class="heading-collapse">Each and every State Government provides Primary, Secondary and Higher Secondary Education in its respective state. Here we listed <h6>best Special Schools</h6> in <?php echo $city; ?> state government.</div>
+                <div class="col">
+                    <div class="footer-list-inner">
+                        <div class="footer-list-icon"><img src="<?php echo base_url() ?>assets/front/images/icons/f-3.png" alt="best kindergarten schools in <?php echo $city; ?>" /></div>
+                        <h5 class="mb-2">Matriculation Schools</h5>
+                        <div class="heading-collapse">Schools use English as the language of instruction. They have a unique curriculum until class 10 and follow the <?php echo $city; ?> State Board curriculum for classes 11 and 12. You can choose <h6>best Matriculation schools in <?php echo $city; ?>.</h6></div>
+                    </div>
                 </div>
 
-                <div class="col-lg-4 mab-20">
-                    <h5 class="mb-2">Kindergarten Schools</h5>
-                    <div class="heading-collapse">Kindergarten program plays an important role for the children, as it helps them to grow emotionally, physically and mentally. Choose <h6>best kindergarten schools</h6> in <?php echo $city; ?> from our list.</div>
+                <div class="col">
+                    <div class="footer-list-inner">
+                        <div class="footer-list-icon"><img src="<?php echo base_url() ?>assets/front/images/icons/f-4.png" alt="best kindergarten schools in <?php echo $city; ?>" /></div>
+                        <h5 class="mb-2">Special Schools</h5>
+                        <div class="heading-collapse">Each and every State Government provides Primary, Secondary and Higher Secondary Education in its respective state. Here we listed <h6>best Special Schools</h6> in <?php echo $city; ?> state government.</div>
+                    </div>
                 </div>
 
-                <div class="col-lg-4 mab-20">
-                    <h4 class="mb-3 mt-3">Keep In Touch</h4>
-                    <ul class="list-inline"> 
-                        <li class="list-inline-item"><a href="https://www.facebook.com/edugatein" target="_blank"><i class="fa fa-facebook"></i></a></li> 
-                        <li class="list-inline-item"><a href="https://twitter.com/edugatein" target="_blank"><i class="fa fa-twitter"></i></a></li>
-                        <li class="list-inline-item"><a href="https://www.linkedin.com/company/edugatein/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-                        <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCyatNY2QIPJgj5Id4QMQeig?view_as=subscriber" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
-                        <li class="list-inline-item"><a href="https://www.instagram.com/edugatein/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-                    </ul>
+                <div class="col">
+                    <div class="footer-list-inner">
+                        <div class="footer-list-icon"><img src="<?php echo base_url() ?>assets/front/images/icons/f-5.png" alt="best kindergarten schools in <?php echo $city; ?>" /></div>
+                        <h5 class="mb-2">Kindergarten Schools</h5>
+                        <div class="heading-collapse">Kindergarten program plays an important role for the children, as it helps them to grow emotionally, physically and mentally. Choose <h6>best kindergarten schools</h6> in <?php echo $city; ?> from our list.</div>
+                    </div>
                 </div>
             </div>
         </div><!-- /content-footer-list -->
-        <hr>
-        <div class="text-center mat-50">
-            <p>&copy; Copyright 2019 Edugatein. All Rights Reserved.</p>
-        </div>
     </div><!-- /container -->
+</div>
+<div class="footer-copyright content-footer-list">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6">
+                <p>&copy; Copyright 2019 Edugatein. All Rights Reserved.</p>
+            </div>
+            <div class="col-md-6 text-right">
+                <ul class="list-inline"> 
+                    <li class="list-inline-item"><a href="https://www.facebook.com/edugatein" target="_blank"><i class="fa fa-facebook"></i></a></li> 
+                    <li class="list-inline-item"><a href="https://twitter.com/edugatein" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/company/edugatein/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCyatNY2QIPJgj5Id4QMQeig?view_as=subscriber" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.instagram.com/edugatein/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                </ul>
+            </div>
+        </div>    
+    </div>        
 </div>
 <!--Footer Section End-->
 <!-- /content-footer -->
